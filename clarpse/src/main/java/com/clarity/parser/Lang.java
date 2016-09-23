1e7e49c5628eb98f7e01831b41406161c81de2cf
Muntazir Fadhel <muntazirfadhel@gmail.com>
2016-09-23 17:48:46 -0400

    fix problems with enum json conversion

--- clarpse/src/main/java/com/clarity/parser/Lang.java
+++ clarpse/src/main/java/com/clarity/parser/Lang.java
@@ -1,5 +1,13 @@
 package com.clarity.parser;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Indicates Clarpse's currently supported languages.
  *
@@ -9,14 +17,26 @@ public enum Lang {
 
     JAVA("java", ".java");
 
+	private static Map<String, Lang> namesMap = new HashMap<String, Lang>(1);
+
+    static {
+        namesMap.put(JAVA.value, JAVA);
+    }
+
     private String value;
     private String fileExt;
 
     private Lang(final String value, final String extension) {
         this.value = value;
         fileExt = extension;
+    } 
+    
+    @JsonCreator
+    public static Lang forValue(String value) {
+        return namesMap.get(StringUtils.lowerCase(value));
     }
 
+    @JsonValue
     public String value() {
         return value;
     }
